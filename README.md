45f98726f9a7000261a9667050f7280a9b57e758
Carlos Carral C <carloscarral13@gmail.com>
2022-02-22 14:16:41 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-6502-emu
+
+Repository for the 6502 emulator front end component written in vue.
---
+++ src/Emulator.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div>Emulator</div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+
+    }
+}
+</script>
