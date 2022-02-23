d4bdaa10fd3c54b92de290fee3056ade5f488c63
Carlos Carral C <carloscarral13@gmail.com>
2022-02-23 09:19:22 -0600

    feature: add editor component

--- src/Emulator.vue
+++ src/Emulator.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
-        <div>Emulator</div>
+        <div id="editor-and-registers">
+            <textarea id="editor" cols="30" rows="10"></textarea>
+            <div id="registers">PC X Y</div>
+        </div>
     </div>
 </template>
 <script>
+
 export default {
     data() {
-
+        return {}
     }
 }
 </script>
+
+<style>
+#editor {
+    width: 250px;
+    height: 300px;
+    resize: none;
+    font-size: 20px;
+    float: left;
+}
+#registers {
+    float: right;
+    font-family: Consolas, sans-serif;
+    font-size: 25px;
+    margin: 10px;
+}
+</style>
